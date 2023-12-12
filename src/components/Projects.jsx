f001669f276676f3b7dac7f7b453deb33c6c7663
Jennifer <jennifer.y.hong@gmail.com>
2023-12-11 22:50:51 -0500

    project descriptions

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,9 +4,12 @@ function Projects() {
   return (
     <div className="Projects">
       <h2 className="sectionHead">Projects and Tech Stack.</h2>
-      <p className="project-single">aMUSE</p>
-      <p className="project-single">Grocery List</p>
-      <p className="project-single">Stranger's Things</p>
+      <p className="project-single">aMUSE
+      A comprehensive museum exploration and review web application. Users can discover nearby museums, read and leave reviews with ratings, access detailed museum information, and maintain private journal entries about their visits.</p>
+      <p className="project-single">Grocery List
+      A functional web application for grocery list management, offering features inspired by popular e-commerce platforms. Users can list, update, delete, and view items seamlessly</p>
+      <p className="project-single">Stranger's Things
+      A functional web application inspired by Craig’s List and eBay, enabling users to list items for sale, perform updates, deletions, and view all listed items. </p>
     </div>
   );
 }
